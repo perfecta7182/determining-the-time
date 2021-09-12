aecfb97bc8d9218dae3600130b4eed4973580f27
perfecta7182 <80309351+perfecta7182@users.noreply.github.com>
2021-09-12 23:06:46 +0600

    det_time

---
+++ определение времени/def.py
@@ -0,0 +1,10 @@
+def say_hello(cur):
+    if cur <=5 or cur >= 23:
+        print('доброй ночи')
+    elif cur <=6 or cur >= 11:
+        print('Доброе утро')
+    elif current_hour >= 12 and current_hour <= 17:
+        print('Добрый день!')
+    elif current_hour >= 18 and current_hour <= 22:
+        print('Добрый вечер!')
+
